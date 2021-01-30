3edd4329b690def7c52251df77e190a860b39a4f
NicolasArayaB <nicolasaraya@gmail.com>
2021-01-30 02:20:12 +0000

    done

--- exercises/05.1-second-styles/index.js
+++ exercises/05.1-second-styles/index.js
@@ -4,12 +4,15 @@ import PropTypes from "prop-types";
 
 const buttonStyles = {
 	//write button styles here
-
+    background: "yellow",
+    border: "none",
+    color: "black"
 };
 
 const badgeStyles = {
 	//write the span styles here
-
+    color: "white",
+    background: "red"
 };
 
 const Badge = props => {
